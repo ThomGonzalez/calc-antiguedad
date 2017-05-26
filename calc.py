f6c805b72e66a52953824be9938e071606633e91
thomgonzalez <thom.sgonzalez@gmail.com>
2017-05-26 12:02:18 -0500

    Validación de año actual si es bisiesto.

--- calc.py
+++ calc.py
@@ -1,6 +1,27 @@
 from datetime import datetime
 
 
+def check_date(year, month, day):
+    correct_date = None
+    try:
+        new_date = datetime(year, month, day)
+        correct_date = True
+    except ValueError:
+        correct_date = False
+    return correct_date
+
+
+def es_bisiesto(anio=None):
+	"""
+	Valida si el año actual es bisiesto.
+	"""
+	bisiesto = False 
+	# Probamos si el mes de febrero del año actual tiene 29 días 
+	if check_date(anio, 2, 29):
+		bisiesto = True
+	return bisiesto
+
+
 def calc_ant_semanas(fecha_inicio, fecha_actual):
 	"""
 	Cálcula antigüedad en semanas.
@@ -30,8 +51,11 @@ def calc_ant_tiempo(fecha_inicio, fecha_actual):
 	dias = int(lst_fact[2]) - int(lst_fini[2])
 
 	dias_mes_anterior = 0
+	anio_actual = int(lst_fact[0])
 	mes_actual = int(lst_fact[1])
-
+	# Sumar días mas uno.
+	dias += 1
+	
 	if dias < 0:
 		meses -= 1
 		# Ahora hay que sumar a $dias los dias que tiene el mes anterior de la fecha actual 
@@ -40,7 +64,10 @@ def calc_ant_tiempo(fecha_inicio, fecha_actual):
 		elif mes_actual == 2:
 			dias_mes_anterior = 31
 		elif mes_actual == 3:
-			dias_mes_anterior = 28
+			if es_bisiesto(anio=anio_actual):
+				dias_mes_anterior = 29
+			else: 
+				dias_mes_anterior = 28
 		elif mes_actual == 4:
 			dias_mes_anterior = 31
 		elif mes_actual == 5:
@@ -73,8 +100,8 @@ def calc_ant_tiempo(fecha_inicio, fecha_actual):
 	return data
 
 
-fecha_inicio = '1989-11-16'
-fecha_actual = '2017-05-26'
+fecha_inicio = '2008-03-31'
+fecha_actual = '2009-03-02'
 
 data = calc_ant_semanas(fecha_inicio, fecha_actual)
 print("Semanas: %s" % data)
